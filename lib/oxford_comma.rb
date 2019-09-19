7813ac9cbe4e3fa03babcec44513372cbc713d2d
Brandon Furr <brandon.furr123@gmail.com>
2019-09-19 15:45:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   new_array = []
-  if new_array.length = 2 
+  if new_array.length[2]
     new_array.join("and")
-  elsif new_array.length = 3
+  elsif new_array.length[3]
   new_array.insert(2, "and")
   new_array.join
 else
