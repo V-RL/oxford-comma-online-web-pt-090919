3e4de29e152cb58424f7d90bc557fb62a7d4dd78
Brandon Furr <brandon.furr123@gmail.com>
2019-09-19 16:00:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,7 @@
 def oxford_comma(array)
   new_array = []
-  if new_array.length[2]
-    new_array.join("and")
-  elsif new_array.length[3]
-  new_array.insert(2, "and")
-  new_array.join
-else
+  when new_array > [0]
+    new_array.insert(.last, "and")
 end
 
 end
