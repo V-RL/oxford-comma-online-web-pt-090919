70cf898dc3cd57192a75683ed2022786a373dae7
Brandon Furr <brandon.furr123@gmail.com>
2019-09-19 17:14:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(people)
     if people.length == 1 
       return "#{people[0]}"
     elsif people.length == 2 
-    return "#{people[0] and #{people[1]}"
+    return "#{people[0]} and #{people[1]}"
     
   end
     
